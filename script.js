8eec87fb7c3f21b35ee728960f1cb09b826b1f55
Rushang2005 <103581843+Rushang2005@users.noreply.github.com>
2024-06-11 22:18:23 +0530

    Add files via upload
    attage all files

---
+++ script.js
@@ -0,0 +1,11 @@
+const title = document.getElementById("title");
+const desc = document.getElementById("desc")
+const sname = document.getElementById("name")
+
+
+
+title.innerText = "My Love 😘, My heart❤️, My Angry Bird😘 ";
+
+desc.innerText = "I cannot tell you how much I love you. You are soo beautiful girl in the world .I hope I never ever loose you and our bond grows stronger and deeper with me.";
+
+sname.innerText = "AHOO";
